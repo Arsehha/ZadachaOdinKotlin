7ed92bdd7c000ed826b224829c85fa6aebb29608
Arsehha <159578130+Arsehha@users.noreply.github.com>
2024-09-09 21:40:05 +0300

    1.2 Compleat

--- src/main/kotlin/K1.2.kt
+++ src/main/kotlin/K1.2.kt
@@ -1,7 +1,12 @@
 fun main () {
     println("Введите символы")
-    //val input = readln()
-    val input = "AASADDSS"
+    val input = readln()
+    //val input = "AASADDSS"
+    var chars = input.toList().distinct().sorted()
+    var result =  ""
 
-
+    for(char in chars) {
+        result += "$char - ${input.count {c -> c ==char}}\n"
+    }
+    println("Результат: \n$result")
 }
